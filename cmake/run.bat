54df9bc3a40380bbce40490a29760faa792bc409
xinlei zhao <zxlwbi199101@gmail.com>
2020-09-22 01:56:39 +0800

    clean up third party library

--- cmake/run.bat
+++ cmake/run.bat
@@ -2,4 +2,4 @@ cmake -B build -D Boost_ROOT=C:\Users\ZhaoXinlei\cpplibs\boost_1_73_0
 
 & 'C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe' .\build\client-cli.sln
 
-.\Debug\client.exe
+.\build\Debug\client.exe
